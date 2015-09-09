f54d671826d90da3256b742b36097ce5814bedbc
Nathan <nhare330@gmail.com>
2015-09-09 16:59:53 -0400

    Replace table name with a variable
    This enables better portability.

--- calendar.php
+++ calendar.php
@@ -1,5 +1,7 @@
 <?php
 
+  $table = "my_table"; //REPLACE my_table WITH THE TABLE THAT WILL BE CHECKED FOR EVENTS
+
   if (isset($_GET['m']) && isset($_GET['y'])) {
     $m = $_GET['m'];
     $y = $_GET['y'];
@@ -20,22 +22,6 @@
   $prevdate = $caldate - 10 * $dateunits['day'];
   $prevm = date('n', $prevdate);
   $prevy = date('Y', $prevdate);
-  /*
-  if ($m == 12) {
-    $nextm = 1;
-    $nexty = $y+1;
-  } else{
-    $nextm = $m+1;
-    $nexty = $y;
-  }
-  if ($m == 1) {
-    $prevm = 12;
-    $prevy = $y-1;
-  } else{
-    $prevm = $m-1;
-    $prevy = $y;
-  }
-*/
 
   echo '<table width=100% cellspacing="0" class="calendar">';
     echo '<tr bgcolor=#72aaff>';
@@ -69,7 +55,7 @@
           while ($daysbefore > 0) {
             $tempday = $prevtotal - $daysbefore + 1;
             $prevdate = date('Y-m-d', strtotime($prevy . '-' . $prevm . '-' . $tempday));
-            $sql = "SELECT * FROM articles WHERE type = 'event' AND eventdate BETWEEN '$prevdate 00:00:00' AND '$prevdate 23:59:59'";
+            $sql = "SELECT * FROM $table WHERE type = 'event' AND eventdate BETWEEN '$prevdate 00:00:00' AND '$prevdate 23:59:59'";
             echo '<td class="cal-other" align="center">' . $tempday . '</td>';
             $daysbefore--; 
           } 
@@ -78,7 +64,7 @@
         $endofday = $caldate + $dateunits['day'] - 1;           
         $formatted = date('Y-m-d', $caldate);
         $today = date('Y-m-d', time());
-        $sql = "SELECT * FROM articles WHERE type = 'event' AND startdate BETWEEN '$caldate' AND '$endofday'";
+        $sql = "SELECT * FROM $table WHERE type = 'event' AND startdate BETWEEN '$caldate' AND '$endofday'";
         $result = mysql_query($sql); 
           if (mysql_num_rows($result) > 0) { 
             echo '<td class="cal-event" align="center">';
