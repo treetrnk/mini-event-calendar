77e2c69cdcb1dd843906e512e5dc896273b39cd7
Nathan <nhare330@gmail.com>
2015-09-17 09:25:14 -0400

    Convert all echoes to double quotes. Add $url variable.

--- calendar.php
+++ calendar.php
@@ -1,7 +1,7 @@
 <?php
 
-  $table = "my_table"; //REPLACE my_table WITH THE TABLE THAT WILL BE CHECKED FOR EVENTS
-
+  include('cal-variables.php')
+  
   if (isset($_GET['m']) && isset($_GET['y'])) {
     $m = $_GET['m'];
     $y = $_GET['y'];
@@ -23,30 +23,40 @@
   $prevm = date('n', $prevdate);
   $prevy = date('Y', $prevdate);
 
-  echo '<table width=100% cellspacing="0" class="calendar">';
-    echo '<tr bgcolor=#72aaff>';
-      echo '<td><a style="text-decoration:none;color:#000000;" href="?page=home&m=' . $prevm . '&y=' . $prevy . '">&#9668;</a></td>';
-      echo "<td colspan='5' align='center'><a style='text-decoration:none;color:#000000;' href='?page=home'><b>$month $y</b></td>";
-      echo "<td align='right'><a style='text-decoration:none;color:#000000;' href='?page=home&m=$nextm&y=$nexty'>&#9658;</a></td>";
-    echo '</tr>';
-    echo '<tr style="border-top: 1px solid #999999;" bgcolor=#aaccff>';
-      echo '<td align="center"><b>Su</b></td>';
-      echo '<td align="center"><b>Mo</b></td>';
-      echo '<td align="center"><b>Tu</b></td>';
-      echo '<td align="center"><b>We</b></td>';
-      echo '<td align="center"><b>Th</b></td>';
-      echo '<td align="center"><b>Fr</b></td>';
-      echo '<td align="center"><b>Sa</b></td>';
-    echo '</tr>';
-    echo '<tr>';
+  echo "
+    <table width=100% cellspacing='0' class='calendar'>
+      <tr bgcolor=#72aaff>
+        <td>
+          <a style='text-decoration:none;color:#000000;' href='" . $url . "m=$prevm&y=$prevy>&#9668;</a>
+        </td>
+        <td colspan='5' align='center'>
+          <a style='text-decoration:none;color:#000000;' href='$url'><b>$month $y</b>
+        </td>
+        <td align='right'>
+          <a style='text-decoration:none;color:#000000;' href='" . $url . "m=$nextm&y=$nexty'>&#9658;</a>
+        </td>
+      </tr>
+      <tr style='border-top: 1px solid #999999;' bgcolor=#aaccff>
+        <th>Su</th>
+        <th>Mo</th>
+        <th>Tu</th>
+        <th>We</th>
+        <th>Th</th>
+        <th>Fr</th>
+        <th>Sa</th>
+      </tr>
+      <tr>
+  ";
     
       $dayofweek = date('w', $caldate);
       $dayofmonth = 1;
       while ($dayofmonth <= $total_days) {          //START FILLING DAYS
         if ($dayofweek == 7) {
           $dayofweek = 0;
-          echo '</tr>';
-          echo '<tr>';
+          echo "
+      </tr>
+      <tr>
+          ";
         }
 
         if ($dayofmonth == 1) {                                       //PREVIOUS MONTH DAYS
@@ -56,7 +66,7 @@
             $tempday = $prevtotal - $daysbefore + 1;
             $prevdate = date('Y-m-d', strtotime($prevy . '-' . $prevm . '-' . $tempday));
             $sql = "SELECT * FROM $table WHERE type = 'event' AND eventdate BETWEEN '$prevdate 00:00:00' AND '$prevdate 23:59:59'";
-            echo '<td class="cal-other" align="center">' . $tempday . '</td>';
+            echo "<td class='cal-other' align='center'>$tempday</td>";
             $daysbefore--; 
           } 
         }  
@@ -67,31 +77,33 @@
         $sql = "SELECT * FROM $table WHERE type = 'event' AND startdate BETWEEN '$caldate' AND '$endofday'";
         $result = mysql_query($sql); 
           if (mysql_num_rows($result) > 0) { 
-            echo '<td class="cal-event" align="center">';
-            echo "<a href='?page=event&date=$formatted'"; 
-            if ($formatted == $today) { echo 'style="border: 1px solid #454545;border-radius: 3px;"'; }
+            echo "<td class='cal-event' align='center'>";
+            echo "<a href='" . $url . "date=$formatted'"; 
+            if ($formatted == $today) { echo "style='border: 1px solid #454545;border-radius: 3px;'"; }
             echo ">$dayofmonth</a>";
           } else {
             echo '<td class="cal-normal" align="center">';
-            if ($formatted == $today) { echo '<span style="border: 1px solid #454545;border-radius: 3px;">'; }
+            if ($formatted == $today) { echo "<span style='border: 1px solid #454545;border-radius: 3px;'>"; }
             echo $dayofmonth;
             //echo mysql_error($result);
-            if ($formatted == $today) { echo '</span>'; }
+            if ($formatted == $today) { echo "</span>"; }
           }
-        echo '</td>'; 
+        echo "</td>"; 
 
         $dayofmonth++;
         $dayofweek++;
       }  
       $tempday = 1;
       while ($dayofweek < 7) {                                                          //NEXT MONTH DAYS
-        echo '<td class="cal-other" align="center">' . $tempday  . '</td>';
+        echo "<td class='cal-other' align='center'>$tempday</td>";
         $dayofweek++;
         $tempday++;
       }
     
-    echo '</tr>';
-  echo '</table>';
+  echo "
+      </tr>
+    </table>
+  ";
 
 
 ?>
