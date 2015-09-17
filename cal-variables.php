ef914f7c9ee4969f057a749adcc55e270da6bfba
Nathan <nhare330@gmail.com>
2015-09-17 10:28:30 -0400

    Add mysql variables.

--- cal-variables.php
+++ cal-variables.php
@@ -1,10 +1,42 @@
 <?php
+
+    ///////////////////////////
+   //    MYSQL VAIRABLES    //
+  ///////////////////////////
+  
+  /*
+  This variable determines whether or not the calendar checks for events. The options 
+  are "yes" or "no".
+  */
+  $check_events = "no";
   
   /*
   Replace my_table with the table that will be checked for events
   */
   $table = "my_table"; 
-
+  
+  /*
+  Enter the table's collumn that is checked for an event date. If the date is on a day
+  being displayed by the calendar, it will highlight it and make it a link. Can only
+  be left blank if $check_events above is set to no.
+  */
+  $date_col = "";
+  
+  /*
+  This variable is for additional delimiters/conditions in the SQL statement. It comes
+  after the WHERE keyword, meaning that it should contain conditions only. This can be 
+  left blank.
+  */
+  $additional_sql = "";
+  
+  
+  
+    ///////////////////////////////
+   //    HYPERLINK VARIABLES    //
+  ///////////////////////////////
+  
+  // NOTE: THESE CAN BE LEFT BLANK
+  
   /*
   Enter your URL in the string below. 
   Note that this variable is placed at the beginning of <A HREF> link destinations in calendar.php.
