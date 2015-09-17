70ad61fad18b884ad0853fe231c038518c1385d6
Nathan <nhare330@gmail.com>
2015-09-17 09:59:48 -0400

    Fix spelling and spacing

--- cal-variables.php
+++ cal-variables.php
@@ -1,7 +1,7 @@
 <?php
   
   /*
-  Replace my_table with the tabel that will be checked for events
+  Replace my_table with the table that will be checked for events
   */
   $table = "my_table"; 
 
@@ -15,14 +15,13 @@
   $url = "";
 
   /*
-  Enter any $_GET variables that you would like to have included in all URLs.
-  Do not end the string with a '&' to accommadate additional variables already in place.
+  Enter any $_GET variables that you would like to have included in all <A HREF> links.
+  Do NOT end the string with a '&' to accommadate additional variables already in place.
   For example, if your website is set up to navigate using PHP, you may
   want to add something like "page=$page".
   Again, like the above variable, here is an example of how it will be called:
   echo "<a href='$url?$getvars&date=$date'>Example</a>";
   */
-  
   $getvars = "";
 
 ?>
